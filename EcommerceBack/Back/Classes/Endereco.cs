adb419de4aa15dd4ea192471c18026c46db57279
joyce-fcosta <joyced.fcosta@gmail.com>
2023-02-08 18:00:51 -0300

    Inicio do back do ecommerce

---
+++ EcommerceBack/Back/Classes/Endereco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro.Classes
+{
+    public class Endereco
+    {
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Cidade { get; set; }
+        public string Bairo { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+
+    }
+}
---
+++ EcommerceBack/Back/Classes/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Cadastro.Classes
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string DataNascimento { get; set; }
+        public string Cpf { get; set; }
+        public string Senha { get; set; }
+        public Endereco MeuEndereco { get; set; } = new Endereco();
+
+    }
+}
